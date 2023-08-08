213d7341bf42382aa3b7dd15e0991e6615fffe95
roy2477 <61226515+roy2477@users.noreply.github.com>
2023-08-08 13:00:45 +0300

    First Working Version

---
+++ README.md
@@ -0,0 +1,35 @@
+
+# Saz2Json
+
+[![MIT License](https://img.shields.io/badge/License-MIT-yellow.svg)](https://choosealicense.com/licenses/mit/)
+[![Python Version](https://img.shields.io/badge/Python-3.x-blue.svg)](https://www.python.org/downloads/)
+
+The "Saz2Json" project is a specialized Python tool tailored for network research purposes. Its primary objective is to facilitate the conversion of Fiddler SAZ files into the JSON format, optimizing the extraction and analysis of captured network traffic data. Designed with a focus on network research, Saz2Json retains crucial request and response information, enabling researchers, developers, and analysts to delve into in-depth network behavior analysis. By bridging the gap between SAZ and JSON, this lightweight and user-friendly tool empowers network researchers to efficiently explore, dissect, and draw insights from network interactions, ultimately enhancing the efficacy of network research endeavors.
+
+## Features
+
+- Converts SAZ files to a folder contains JSON format files.
+- Retains request and response details.
+- Lightweight and easy-to-use.
+
+## Usage
+
+```bash
+python Saz2Json.py <saz_file_path> <output_folder>
+```
+
+- <saz_file_path> is the path to the .saz file that you wish to convert.
+- <output_folder> is the name of the target folder that will contain the JSON files (file per session).
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE](https://choosealicense.com/licenses/mit/) file for details.
+
+## Contributing
+
+Contributions are welcome! If you find any issues or have suggestions for improvements, feel free to open an issue or submit a pull request.
+
+## Authors
+
+- [@RoyElia](https://www.linkedin.com/in/roy-elia/)
+
---
+++ Saz2Json.py
@@ -0,0 +1,116 @@
+import os
+import sys
+import datetime
+import json
+from zipfile import ZipFile
+from pytz import timezone
+
+# Defaults
+domains = []
+saz_file_path = ''
+output_folder = ''
+my_timezone = 'GMT'
+time_sent_format = '%Y-%m-%d-%H:%M:%S'
+date_format = '%a, %d %b %Y %H:%M:%S ' + my_timezone
+
+
+def parse_headers(content):
+    # Get rid of the body
+    headers = content.split(b'\x0d\x0a\x0d\x0a')[0]
+    # Split by lines without the first one (eg. 'HTTP/1.1 200')
+    headers_lines = headers.splitlines()[1:]
+
+    parsed_headers = []
+    time_sent = ''
+
+    for line in headers_lines:
+        if b': ' in line:
+            line = line.decode('utf-8')
+            split_header = line.split(': ')
+            parsed_headers.append(split_header)
+
+            # Retrieve time from relevant header
+            if split_header[0] == 'Time-Sent':
+                time_sent = datetime.datetime.fromtimestamp(int(split_header[1]) / 1000, tz=timezone(my_timezone))\
+                    .strftime(time_sent_format)
+            elif split_header[0] == 'Date':
+                time_sent = datetime.datetime.strptime(split_header[1], date_format)
+                time_sent = time_sent.strftime(time_sent_format)
+
+    return parsed_headers, time_sent
+
+
+def parse_body(content):
+    # Get rid of the headers
+    body = content.split(b'\x0d\x0a\x0d\x0a')[1:]
+    body = b'\x0d\x0a\x0d\x0a'.join(body).hex()
+
+    return body
+
+
+def parse_saz_file(file_path, output_folder):
+    # Create output folder if it doesn't exist
+    if not os.path.exists(output_folder):
+        os.makedirs(output_folder)
+
+    with ZipFile(file_path, 'r') as saz_zip:
+        # Extract the sessions files from the .saz archive
+        session_files = [f for f in saz_zip.namelist() if f.endswith('.txt')]
+        session_files.reverse()
+        session_tuples = []
+        while len(session_files) > 1:
+            session_tuples.append({'request': session_files.pop(), 'response': session_files.pop()})
+
+        for session_tuple in session_tuples:
+            session_data = {'domain': '', 'path': '', 'time': '', 'parsed_request': {}, 'parsed_response': {}}
+
+            # Parse request
+            with saz_zip.open(session_tuple['request']) as req:
+                content = req.read()
+                session_data['request_hex'] = content.hex()
+                session_data['parsed_request']['req_type'] = content.split(b'\r\n')[0].split(b' ')[0].decode('utf-8')
+                if session_data['parsed_request']['req_type'] == 'CONNECT':
+                    continue
+                path = content.split(b'\r\n')[0].split(b' ')[1]
+                session_data['domain'] = path.split(b'://')[1].split(b'/')[0].decode('utf-8')
+                path = path.split(b'://')[1].split(b'/')[1:]
+                session_data['path'] = (b'/' + b'/'.join(path)).decode('utf-8')
+                if len(domains) > 0 and session_data['domain'] not in domains:
+                    continue
+
+                session_data['parsed_request']['headers'], time_sent = parse_headers(content)
+                if time_sent != '':
+                    session_data['time'] = time_sent
+                session_data['parsed_request']['body_hex'] = parse_body(content)
+
+            # Parse response
+            with saz_zip.open(session_tuple['response']) as res:
+                content = res.read()
+                session_data['response_hex'] = content.hex()
+
+                session_data['parsed_response']['code'] = int(content.split(b'\r\n')[0].split(b' ')[1])
+
+                session_data['parsed_response']['headers'], time_sent = parse_headers(content)
+                if time_sent != '' and session_data['time'] == '':
+                    session_data['time'] = time_sent
+                session_data['parsed_response']['body_hex'] = parse_body(content)
+
+            # Dump session data to a file
+            filename = 'session' + res.name.split('raw/')[1].split('_')[0] + '.json'
+            file_path = os.path.join(output_folder, filename)
+
+            json_object = json.dumps(session_data, indent=4)
+            with open(file_path, 'w') as json_file:
+                json_file.write(json_object)
+
+
+# Check if all arguments are provided
+if len(sys.argv) != 3:
+    print("[!] Usage: python " + sys.argv[0] + " <saz_file_path> <output_folder>")
+    exit()
+
+else:
+    saz_file_path = sys.argv[1]
+    output_folder = sys.argv[2]
+
+parse_saz_file(saz_file_path, output_folder)
